ce52b2c56e25a4bce5ca978130521c77c7b74920
baichen318@gmail.com <baichen318@gmail.com>
2020-04-22 17:01:08 +0800

    Add basic types inherited from HeteroCL

--- include/tvm/tir/stmt.h
+++ include/tvm/tir/stmt.h
@@ -508,7 +508,8 @@ enum class ForType : int {
   /*! \brief Vector SIMD loop annotaion. */
   Vectorized = 2,
   /*! \brief Unroll annotation. */
-  Unrolled = 3
+  Unrolled = 3,
+  Pipelined = 4
 };
 
 // Kevice api of for loop
